7e4f94f9165fba2c948ef14cdbe600c817cb7161
Edinei <galera.org@gmail.com>
2019-01-15 17:55:49 -0200

    grid list route

---
+++ public/assets/listagem.js
@@ -0,0 +1,19 @@
+if(typeof loadListagem === "undefined") {
+    function loadListagem() {
+        let entityListagem = app.route.replace("listagem/", "");
+        if (entityListagem.indexOf('/') > -1) {
+            let t = entityListagem.split('/');
+            entityListagem = t[0];
+        }
+
+        //entity title
+        let p = new RegExp(/s$/i);
+        let title = ucFirst((p.test(entityListagem) ? entityListagem.substr(0, (entityListagem.length - 1)) : entityListagem).replaceAll('_', ' ').replaceAll('-', ' '));
+        $("#core-content").find("h4").html(title);
+
+        //form
+        $("#listagem").grid(entityListagem);
+    }
+}
+
+loadListagem();
---
+++ public/view/listagem.php
@@ -0,0 +1,10 @@
+<div class="col font-light theme-d2 theme-border-l margin-bottom" style="border-bottom: solid 5px">
+    <div class="container-1200">
+        <h4 class="panel padding-32"></h4>
+    </div>
+</div>
+<div class="container-1200">
+    <div id="listagem" class="col"></div>
+</div>
+
+<div class="col padding-24"></div>
