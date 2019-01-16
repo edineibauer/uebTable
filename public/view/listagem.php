3ec0551c5c2bf90bc2fd4c053a8e4e41109709a0
Edinei <galera.org@gmail.com>
2019-01-16 10:09:41 -0200

    icon in grid list and min-width table

--- public/view/listagem.php
+++ public/view/listagem.php
@@ -1,6 +1,11 @@
 <div class="col font-light theme-d2 theme-border-l" style="border-bottom: solid 5px">
     <div class="container-1200">
-        <h4 class="panel padding-32"></h4>
+        <div class="panel padding-32" id="formulario-header">
+            <span class="left color-gray-light btn-floating">
+                <i class="material-icons left padding-right">view_list</i>
+            </span>
+            <h4 class="left padding-8 padding-left"></h4>
+        </div>
     </div>
 </div>
 <div class="container-1200">
