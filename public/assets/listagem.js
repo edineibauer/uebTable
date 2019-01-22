bd6d587fb8fcd15ab684b79d6ba3c077cbe1d08a
Edinei <galera.org@gmail.com>
2019-01-22 17:55:07 -0200

    list request entity in dicionary

--- public/assets/listagem.js
+++ public/assets/listagem.js
@@ -6,14 +6,23 @@ if(typeof loadListagem === "undefined") {
             entityListagem = t[0];
         }
 
-        //entity title
-        let p = new RegExp(/s$/i);
-        let title = ucFirst((p.test(entityListagem) ? entityListagem.substr(0, (entityListagem.length - 1)) : entityListagem).replaceAll('_', ' ').replaceAll('-', ' '));
-        $("#core-content").find("h4").html(title + " <small class='opacity'> >> listagem</small>");
+        dbLocal.exeRead('__dicionario', 1).then(dic => {
+            for(var i in dic) {
+                if(i === entityListagem) {
 
-        //form
-        $("#listagem").html("").grid(entityListagem);
-        $.cachedScript(HOME + "assetsPublic/tableCore.min.js");
+                    //entity title
+                    let p = new RegExp(/s$/i);
+                    let title = ucFirst((p.test(entityListagem) ? entityListagem.substr(0, (entityListagem.length - 1)) : entityListagem).replaceAll('_', ' ').replaceAll('-', ' '));
+                    $("#core-content").find("h4").html(title + " <small class='opacity'> >> listagem</small>");
+
+                    //form
+                    $("#listagem").html("").grid(entityListagem);
+                    $.cachedScript(HOME + "assetsPublic/tableCore.min.js");
+
+                    return false;
+                }
+            }
+        });
     }
 }
 
