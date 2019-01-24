e84266834dedc571c2e4386f3a4e3bb772295447
Edinei <galera.org@gmail.com>
2019-01-24 09:14:24 -0200

    grid full size

--- public/view/listagem.php
+++ public/view/listagem.php
@@ -1,2 +1,4 @@
-<div id="listagem" class="col"></div>
-<div class="col padding-24"></div>
+<div class="container-1200" style="display: flow-root">
+    <div id="listagem" class="col"></div>
+    <div class="col padding-24"></div>
+</div>
