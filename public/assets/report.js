08d7999dfbc95624b6ccea4ad4089d86205a9f51
Edinei J. Bauer <galera.org@gmail.com>
2020-05-03 11:39:50 -0300

    add function async readReport

--- public/assets/report.js
+++ public/assets/report.js
@@ -8,49 +8,40 @@
  * @param offset
  * @returns {PromiseLike<any> | Promise<any>}
  */
-function reportRead(entity, filter, order, reverse, limit, offset) {
+async function reportRead(entity, filter, order, reverse, limit, offset) {
     order = typeof order === "string" ? order : "id";
     reverse = (typeof reverse !== "undefined" ? (reverse ? !0 : !1) : !1);
     limit = parseInt(typeof limit === "number" ? limit : (localStorage.limitGrid ? localStorage.limitGrid : 15));
     limit = limit < parseInt(localStorage.limitGrid) ? parseInt(localStorage.limitGrid) : limit;
     offset = parseInt((typeof offset === "number" ? offset : 0) - 1);
 
-    return db.exeRead(entity).then(data => {
-        if (!SERVICEWORKER && isEmpty(filter))
-            return data;
-
-        /**
-         * Se tiver mais resultados no back que não estão no front
-         * Ou se tiver filtros a serem aplicados
-         * Ou se não estiver trabalhando com uma base front
-         * então faz a leitura online
-         */
-        if (parseInt(data.length) >= parseInt(LIMITOFFLINE) || !isEmpty(filter)) {
-            return new Promise(function (resolve, reject) {
-                $.ajax({
-                    type: "POST",
-                    url: HOME + 'set',
-                    data: {
-                        lib: "entity",
-                        file: "load/report",
-                        entity: entity,
-                        filter: filter,
-                        order: order,
-                        reverse: reverse,
-                        limit: limit,
-                        offset: offset
-                    },
-                    success: function (dados) {
-                        if(dados.response === 1)
-                            resolve({data: dados.data.data, length: dados.data.total});
-                    },
-                    error: () => resolve(readOffline(data, filter, order, reverse, limit, offset)),
-                    dataType: "json"
-                })
-            })
-        } else {
-            return readOffline(data, filter, order, reverse, limit, offset)
-        }
+    /**
+     * Se tiver mais resultados no back que não estão no front
+     * Ou se tiver filtros a serem aplicados
+     * Ou se não estiver trabalhando com uma base front
+     * então faz a leitura online
+     */
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "POST",
+            url: HOME + 'set',
+            data: {
+                lib: "report",
+                file: "read/report",
+                entity: entity,
+                filter: filter,
+                order: order,
+                reverse: reverse,
+                limit: limit,
+                offset: offset
+            },
+            success: function (dados) {
+                if (dados.response === 1)
+                    resolve({data: dados.data.data, length: dados.data.total});
+            },
+            error: () => resolve(readOffline(data, filter, order, reverse, limit, offset)),
+            dataType: "json"
+        })
     })
 }
 
