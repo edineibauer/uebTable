dfb20a83f99a2c793d41d057677275e44da18017
edineijacobbauer <edineibauer@gmail.com>
2021-11-26 18:27:13 -0300

    fix total register

--- public/view/table/get/totalRegistros.php
+++ public/view/table/get/totalRegistros.php
@@ -9,7 +9,7 @@ $entidade = $variaveis[0];
 $setor = Config::getSetor();
 $permissoes = $setor !== "admin" ? Config::getPermission($setor) : [];
 if ($setor === "admin" || empty($permissoes[$entidade]) || (isset($permissoes[$entidade]['read']) && $permissoes[$entidade]['read'])) {
-    $sql = new \Conn\SqlCommand(true, true);
+    $sql = new \Conn\SqlCommand();
     $sql->exeCommand("SELECT COUNT(*) as total FROM " . PRE . $entidade);
     $data['data'] = (int) ($sql->getResult() ? $sql->getResult()[0]['total'] : 0);
 }
