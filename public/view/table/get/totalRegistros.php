b0c0868a193c2a14d580f24511279f5df0ea67e9
edineijacobbauer <edineibauer@gmail.com>
2021-12-10 17:28:17 -0300

    total register with explore permission

--- public/view/table/get/totalRegistros.php
+++ public/view/table/get/totalRegistros.php
@@ -10,6 +10,6 @@ $setor = Config::getSetor();
 $permissoes = $setor !== "admin" ? Config::getPermission($setor) : [];
 if ($setor === "admin" || empty($permissoes[$entidade]) || (isset($permissoes[$entidade]['read']) && $permissoes[$entidade]['read'])) {
     $sql = new \Conn\SqlCommand();
-    $sql->exeCommand("SELECT COUNT(*) as total FROM " . PRE . $entidade);
+    $sql->exeCommand("SELECT COUNT(*) as total FROM " . PRE . $entidade . (!isset($permissoes[$entidade]["explore"]) || !$permissoes[$entidade]["explore"] ? " WHERE system_id = {$_SESSION["userlogin"]["system_id"]}" : ""));
     $data['data'] = (int) ($sql->getResult() ? $sql->getResult()[0]['total'] : 0);
 }
