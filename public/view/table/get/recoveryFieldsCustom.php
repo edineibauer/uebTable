d0fc7cb3476ad13c3604e9691662bb3d90e07d32
Edinei J. Bauer <galera.org@gmail.com>
2024-09-23 17:28:07 -0300

    path private to _cdn

--- public/view/table/get/recoveryFieldsCustom.php
+++ public/view/table/get/recoveryFieldsCustom.php
@@ -4,5 +4,5 @@ $type = $variaveis[0];
 $entity = $variaveis[1];
 
 $data['data'] = [];
-if(file_exists(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"))
-    $data['data'] = json_decode(file_get_contents(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"), !0);
+if(file_exists(PATH_PRIVATE . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"))
+    $data['data'] = json_decode(file_get_contents(PATH_PRIVATE . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"), !0);
--- public/view/table/post/saveFieldsGrid.php
+++ public/view/table/post/saveFieldsGrid.php
@@ -7,10 +7,10 @@ $fields = [];
 foreach ($fieldsn as $item)
     $fields[] = $item;
 
-\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/fieldsCustom");
-\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/fieldsCustom/{$entity}");
-\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}");
+\Helpers\Helper::createFolderIfNoExist(PATH_PRIVATE . "_cdn/fieldsCustom");
+\Helpers\Helper::createFolderIfNoExist(PATH_PRIVATE . "_cdn/fieldsCustom/{$entity}");
+\Helpers\Helper::createFolderIfNoExist(PATH_PRIVATE . "_cdn/fieldsCustom/{$entity}/{$type}");
 
-$f = fopen(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json", "w+");
+$f = fopen(PATH_PRIVATE . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json", "w+");
 fwrite($f, json_encode($fields));
 fclose($f);
